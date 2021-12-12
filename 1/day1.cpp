9d109d3b9612c759c56fc47feeddffa081bedd0d
daria <61759834+jezowska@users.noreply.github.com>
2021-12-12 22:02:50 +0100

    update day1.cpp

--- 1/day1.cpp
+++ 1/day1.cpp
@@ -9,12 +9,13 @@ int main()
     vector<int> input;
     string filename;
     int inc = 0, dec = 0, in = 0;
+    int incThree = 0, decThree = 0;
 
     cout << "File name is: ";
     cin >> filename; 
 
     we.open(filename);
-
+    //Part one
     while(we)
     {
         we >> in;
@@ -23,8 +24,18 @@ int main()
         else dec++;
     }
 
+    //Part two
+    for(int i = 0; i < input.size() - 3; i++)
+    {
+        if(input[i] + input[i+1] + input[i+2] < input[i+1] + input[i+2] + input[i+3]) incThree ++;
+        else decThree ++;
+    }
+
     cout << "Increasing: " << inc << endl;
     cout << "Decreasing: " << dec << endl;
+
+    cout << "Increasing (3 signals): " << incThree << endl;
+    cout << "Decreasing (3 signals): " << decThree << endl;
     
 
 }
