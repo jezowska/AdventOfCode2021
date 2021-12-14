0a902ef81ba70145c32a095803164e09441df85b
daria <61759834+jezowska@users.noreply.github.com>
2021-12-14 14:20:15 +0100

    fixed

--- Day 1/day1.cpp
+++ Day 1/day1.cpp
@@ -1,3 +1,11 @@
+/*
+ * ADVENT OF CODE 2021
+ * DAY 2
+ * https://adventofcode.com/2021/day/1
+ * 
+ * Daria Jeżowska, 252731 
+ */;
+
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -5,37 +13,65 @@ using namespace std;
 
 int main()
 {
+    //zmienne potrzebne do przechowywania wczytanych danych
     ifstream we;
     vector<int> input;
     string filename;
-    int inc = 0, dec = 0, in = 0;
-    int incThree = 0, decThree = 0;
+    int in = 0;
+
+    //zmienne przechowujace informacje o ilosci zwiekszajacych oraz zmniejszajacych sie po kolei liczb 
+    //odpowiednio dla pierwszej i drugiej czesci zadania
+    int increase = 0, decrease = 0;
 
     cout << "File name is: ";
     cin >> filename; 
-
     we.open(filename);
-    //Part one
-    while(we)
+    
+    if(we.fail()) 
     {
+        cout << "Wrong file name" << endl;
+        return 0;
+    }
+
+    //Part one
+    while(!we.eof())
+    {
+       /* 
+        * wczytujemy po kolei zmienne z pliku txt do wektora
+        * następnie sprawdzamy czy wczytaliśmy więcej liczb niż jedną oraz 
+        * czy liczba aktualnie wczytana jest większa niż poprzednia
+        * jeśli tak - zmienna increase większa się o 1
+        * w innym przypadku zwiększa się o 1 zmienna decrease
+        */
+
         we >> in;
         input.push_back(in);
-        if(input.size() > 1 && input[input.size() - 1] > input[input.size() - 2]) inc++;
-        else dec++;
+        if(input.size() > 1 && input[input.size() - 1] > input[input.size() - 2]) increase++;
+        else decrease++;
     }
+
+    cout << "Increasing: " << increase << endl;
+    cout << "Decreasing: " << decrease << endl;
+
+    increase = 0;
+    decrease = 0;
 
     //Part two
     for(int i = 0; i < input.size() - 3; i++)
     {
-        if(input[i] + input[i+1] + input[i+2] < input[i+1] + input[i+2] + input[i+3]) incThree ++;
-        else decThree ++;
+       /*
+        * Sprawdzamy czy suma trzech liczb jest większa od sumy trzech liczb 
+        * zaczynając od liczby kolejnej od naszej startowej
+        * Jeśli tak jest - zmienna increase zwiększa się 
+        * W innym przypadku zwiększa się liczba decreased
+        */
+
+        if(input[i] + input[i+1] + input[i+2] < input[i+1] + input[i+2] + input[i+3]) increase ++;
+        else decrease ++;
     }
 
-    cout << "Increasing: " << inc << endl;
-    cout << "Decreasing: " << dec << endl;
-
-    cout << "Increasing (3 signals): " << incThree << endl;
-    cout << "Decreasing (3 signals): " << decThree << endl;
+    cout << "Increasing (3 signals): " << increase << endl;
+    cout << "Decreasing (3 signals): " << decrease << endl;
     
 
 }
--- Day 2/day2.py
+++ Day 2/day2.py
@@ -1,33 +1,41 @@
-depth = 0
-depth2 = 0
+### ADVENT OF CODE 2021 ###
+### DAY 2 ###
+
+# https://adventofcode.com/2021/day/2 
+# Daria Jeżowska, 252731
+
+
+depth = 0 
+depth2 = 0 #zmienna depth na potrzeby drugiego zadania
 forward = 0
-aim = 0
 
 ### PART ONE ###
+#zmiena depth z pierwszej części zadania jest tak naprawdę tym samym co zmienna aim w drugim
+# z tego powodu nie tworzyłam zmiennej aim
+
 with open("input.txt") as file:
     for line in file:
         line = line.split(" ")
         
         if(line[0] == 'down'): 
             depth += int(line[1])
-            aim   += int(line[1])
         elif(line[0] == 'forward'):
             forward +=int(line[1])
-            depth2 += aim*int(line[1])
+            depth2 += depth*int(line[1])
         elif(line[0] == 'up'):
             depth -= int(line[1])
-            aim   -= int(line[1])
     
 print("Part one:")
 print("Depth: " + str(depth))
 print("Forward: " + str(forward))
 
 ### PART TWO ###
+
 print("\n")
 print("Part two")
 print("Depth: " + str(depth2))
 print("Forward: " + str(forward))
-print("Aim: " + str(aim))
+print("depth: " + str(depth))
 
         
     
